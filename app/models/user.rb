a5ecb2cde19a9459a7d06970c66deb86c8c73be2
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-04-29 05:47:25 +0000

    user join

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 7 }
+  
+  mount_uploader :image, ImageUploader
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'join', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
 end
