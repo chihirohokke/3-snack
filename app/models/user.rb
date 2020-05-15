acb8e4ac3c7cde8f1540374eb1ccb57a058e0c6b
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-05-15 22:29:03 +0000

    fix delete post

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :sweets, dependent: :destroy, through: :likes, source: :post
   
   has_many :comments, dependent: :destroy
