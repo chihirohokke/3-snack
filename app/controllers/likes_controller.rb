5d05f91f8fff9f6e572e5ae5a0112387e734e5e7
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-05-16 22:25:59 +0000

    Ajax iine

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,16 +2,24 @@ class LikesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    post = Post.find(params[:post_id])
-    current_user.sweet(post)
-    flash[:success] = '投稿にSweetしました。'
-    redirect_back(fallback_location: posts_path)
+    @post = Post.find(params[:post_id])
+    unless current_user.like?(@post)
+      current_user.sweet(@post)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end    
+    end
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    current_user.notsweet(post)
-    flash[:success] = '投稿のSweetを解除しました。'
-    redirect_back(fallback_location: posts_path)
+    @post = Post.find(params[:post_id])
+    if current_user.like?(@post)
+      current_user.notsweet(@post)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 end
