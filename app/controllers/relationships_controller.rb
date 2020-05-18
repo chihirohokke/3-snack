81fc8d6216d55f927a9972abe3ff6b190e1e514e
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-05-18 08:16:10 +0000

    Finish ajax follow-button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,16 +2,24 @@ class RelationshipsController < ApplicationController
   before_action :require_user_logged_in
 
   def create
-    user = User.find(params[:follow_id])
-    current_user.follow(user)
-    flash[:success] = 'ユーザをフォローしました。'
-    redirect_back(fallback_location: users_path)
+    @user = User.find(params[:follow_id])
+    unless current_user.following?(@user)
+      current_user.follow(@user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    current_user.unfollow(user)
-    flash[:success] = 'ユーザのフォローを解除しました。'
-    redirect_back(fallback_location: users_path)
+    @user = User.find(params[:follow_id])
+    if current_user.following?(@user)
+      current_user.unfollow(@user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,9 @@ class User < ApplicationRecord
   
   has_many :comments, dependent: :destroy
   
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverses_of_relationship, source: :user
 
   def sweet(post) 
